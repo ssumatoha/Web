6d41d6ac433f6f0f901c6a7bd4035c61202fcb44
Alexandr <ssumatoha@gmail.com>
2022-03-19 11:21:41 +0300

    first commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-function hi() {
-    alert(хай)
+function onClickButtonTwo() {
+    alert('Приветствую вас, ' + document.getElementById('inp').value)
+}
+
+function onClickButton(el) {
+    document.getElementById(el).innerHTML = '<button onclick="onClickButtonTwo()">Оправить</button><input id="inp" value="Как Ваше имя?"></input>'
 }
